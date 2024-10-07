ab6c73293b39c1934f25016be086a17870424918
ijaz khan <88775028+IjazkhanAfridi@users.noreply.github.com>
2024-10-07 08:14:17 +0300

    add latest changes

--- controllers/discenteController.js
+++ controllers/discenteController.js
@@ -25,7 +25,38 @@ const createDiscente = async (req, res) => {
   }
 };
 
+const updateDiscente = async (req, res) => {
+  const { id } = req.params;
+  const updateFields = req.body;
+
+  try {
+    const updatedDiscente = await Discente.findByIdAndUpdate(
+      id,
+      { $set: updateFields },
+      { new: true, runValidators: true } 
+    );
+
+    if (!updatedDiscente) {
+      return res.status(404).json({ message: 'Discente non trovato' });
+    }
+
+    res.status(200).json(updatedDiscente);
+  } catch (error) {
+    console.error("Errore durante l'aggiornamento del discente:", error);
+    res.status(500).json({ message: 'Errore durante aggiornamento del discente' });
+  }
+};
+
 // Funzione per ottenere tutti i discenti
+const getUserDiscentiById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const discenti = await Discente.findOne({ _id:id }).populate('userId'); // Trova tutti i discenti associati all'utente
+    res.status(200).json(discenti);
+  } catch (error) {
+    res.status(500).json({ message: 'Errore durante il recupero dei discenti' });
+  }
+};
 const getUserDiscenti = async (req, res) => {
   try {
     const discenti = await Discente.find({ userId: req.user.id }).populate('userId'); // Trova tutti i discenti associati all'utente
@@ -36,7 +67,7 @@ const getUserDiscenti = async (req, res) => {
 };
 const getAllDiscenti = async (req, res) => {
   try {
-    const discenti = await Discente.find().populate('userId'); // Trova tutti i discenti associati all'utente
+    const discenti = await Discente.find().populate('userId'); 
     console.log('discenti: ', discenti);
     res.status(200).json(discenti);
   } catch (error) {
@@ -44,4 +75,4 @@ const getAllDiscenti = async (req, res) => {
   }
 };
 
-module.exports = { createDiscente, getAllDiscenti,getUserDiscenti };  // Esporta entrambe le funzioni
+module.exports = { createDiscente, getAllDiscenti,getUserDiscenti,updateDiscente,getUserDiscentiById };  // Esporta entrambe le funzioni
--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -3,10 +3,12 @@ const auth = require('../middleware/auth');
 const {
   getNotifications,
   markNotificationAsRead,
+  deleteNotification,
 } = require('../controllers/notificationController');
 const router = express.Router();
 
 router.get('/', auth, getNotifications);
 router.patch('/:id/read', auth, markNotificationAsRead);
+router.delete('/:id', auth, deleteNotification);
 
 module.exports = router;
