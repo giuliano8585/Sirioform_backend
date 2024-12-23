c500406176fc911906c29b4d566f7b52250c744b
IjazkhanAfridi <iafridi640@gmail.com>
2024-12-23 23:46:53 +0500

    add changaes

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,4 +1,5 @@
 const mailgun = require('mailgun-js');
+require('dotenv').config();
 const DOMAIN = process.env.MAILGUN_DOMAIN;
 const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN});
 
